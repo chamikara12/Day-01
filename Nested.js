16ab9de592aa9633c0b52b8e43247cd5d223c06c
Chamikara Madushanak <chamikaramadushanka55@gmail.com>
2025-03-18 09:36:39 -0800

    Nested
    Print a nested array

---
+++ Nested.js
@@ -0,0 +1,9 @@
+//print the nested array
+
+let nestedArray = [[1,2,3],[5,6],[8,5,3]];
+for(let i=0; i<nestedArray.length; i++){
+    for(let j=0; j<nestedArray[i].length; j++){
+        console.log(nestedArray[i][j]);
+        
+    }console.log();
+}
