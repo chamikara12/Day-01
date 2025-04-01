9eb81429c3a66759c102c7e487df45627bfda7cf
Chamikara Madushanak <chamikaramadushanka55@gmail.com>
2025-04-01 09:23:57 +0530

    Project-lock.json

---
+++ TestIndex.java
@@ -0,0 +1,8 @@
+public class TestIndex{
+	public static void main(String[] args) {
+		index ob1 = new index("Chamikara",23,"Hambantota");
+		System.out.println(ob1.name);
+		System.out.println(ob1.age);
+		System.out.println(ob1.city);
+	}
+}
